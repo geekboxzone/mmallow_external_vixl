8fcae179b1449176c5780eb0ba57f75654ec4c80
Serban Constantinescu <serban.constantinescu@arm.com>
2014-05-06 16:42:55 +0100

    VIXL: Add Android build support
    * Android.mk file and logcat support.
    * MODULE_LICENSE
    * Few fixes for the Android build system
    * Clang fixes from Ian
    
    Note: cctest_vixl will build a 64bit static binary (vixl's native
    tests).
    
    Change-Id: I8b267e778eabfdf8b05e0d6bc8cc94fe21420e83
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

---
+++ Android.mk
@@ -0,0 +1,128 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+#
+# Copyright (c) 2014 ARM Ltd
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the company may not be used to endorse or promote
+#    products derived from this software without specific prior written
+#    permission.
+#
+# THIS SOFTWARE IS PROVIDED BY ARM LTD ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL ARM LTD BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+LOCAL_PATH:= $(call my-dir)
+
+vixl_include_files := $(LOCAL_PATH)/src/ \
+
+vixl_src_files := \
+	src/a64/assembler-a64.cc \
+	src/a64/cpu-a64.cc \
+	src/a64/debugger-a64.cc \
+	src/a64/decoder-a64.cc \
+	src/a64/disasm-a64.cc \
+	src/a64/instructions-a64.cc \
+	src/a64/instrument-a64.cc \
+	src/a64/macro-assembler-a64.cc \
+	src/a64/simulator-a64.cc \
+	src/utils-vixl.cc
+
+vixl_test_files := \
+	test/cctest.cc \
+	test/test-utils-a64.cc \
+	test/test-assembler-a64.cc \
+	test/test-simulator-a64.cc \
+	test/test-disasm-a64.cc \
+	test/test-fuzz-a64.cc
+
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra -DUSE_SIMULATOR
+LOCAL_CPPFLAGS_arm64 := -UUSE_SIMULATOR
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_src_files)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libvixl
+include external/stlport/libstlport.mk
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra -DDEBUG=1  -DUSE_SIMULATOR
+LOCAL_CPPFLAGS_arm64 := -UUSE_SIMULATOR
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_src_files)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libvixlg
+include external/stlport/libstlport.mk
+include $(BUILD_SHARED_LIBRARY)
+
+ifeq ($(WITH_HOST_DALVIK),true)
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra -DUSE_SIMULATOR
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_src_files)
+LOCAL_STATIC_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libvixl
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+# cctest_vixl: VIXL native tests (to run all tests execute ./cctest_vixl --run_all)
+#
+# We only support 64bit host builds of cctest. For this we hardcode -m64 to the
+# compiler/linker flags.
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra -DUSE_SIMULATOR -DDEBUG=1 -UNDEBUG -m64
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_test_files) $(vixl_src_files)
+LOCAL_MODULE_TAGS := optional
+LOCAL_LDFLAGS := -m64
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE := cctest_vixl
+include $(BUILD_HOST_EXECUTABLE)
+endif
--- src/a64/macro-assembler-a64.h
+++ src/a64/macro-assembler-a64.h
@@ -1230,18 +1230,21 @@ class MacroAssembler : public Assembler {
 class InstructionAccurateScope {
  public:
   explicit InstructionAccurateScope(MacroAssembler* masm)
-      : masm_(masm), size_(0) {
+      : masm_(masm) {
     masm_->BlockLiteralPool();
 #ifdef DEBUG
+    size_ = 0;
     old_allow_macro_instructions_ = masm_->AllowMacroInstructions();
     masm_->SetAllowMacroInstructions(false);
 #endif
   }
 
   InstructionAccurateScope(MacroAssembler* masm, int count)
-      : masm_(masm), size_(count * kInstructionSize) {
+      : masm_(masm) {
+    USE(count);
     masm_->BlockLiteralPool();
 #ifdef DEBUG
+    size_ = count * kInstructionSize;
     masm_->bind(&start_);
     old_allow_macro_instructions_ = masm_->AllowMacroInstructions();
     masm_->SetAllowMacroInstructions(false);
@@ -1260,8 +1263,8 @@ class InstructionAccurateScope {
 
  private:
   MacroAssembler* masm_;
-  uint64_t size_;
 #ifdef DEBUG
+  uint64_t size_;
   Label start_;
   bool old_allow_macro_instructions_;
 #endif
